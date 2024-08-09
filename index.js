5b41b6b1228f50f2189f64fa1be1cc94f21546ba
RamFarid <workprojects22@gmail.com>
2024-08-10 02:48:00 +0300

    Adjust the command to be without to keyword

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ bot.start((ctx) => {
   ctx.reply(
     `Welcome ${
       ctx.chat.first_name || 'Bro'
-    }!\nSend me a link with retiming command to retiming the YouTube link like this:\n<b>/retiming https://youtu.be/xxxxxxxxxxx <b>to</b> 1m2s</b>\nIn the previous example I will send you a new link that directive you to the first minute and the 2th second\n<b>Read the following: </b>\n• hour = h \n• minute = m \n• second = s`,
+    }!\nSend me a link with retiming command to retiming the YouTube link like this:\n<b>/retiming https://youtu.be/xxxxxxxxxxx 1m2s</b>\nIn the previous example I will send you a new link that directive you to the first minute and the 2th second\n<b>Read the following: </b>\n• hour = h \n• minute = m \n• second = s`,
     { parse_mode: 'HTML' }
   )
 })
 
 bot.command('retiming', async (ctx) => {
   const msg = ctx.message.text.replace('/retiming', '').trim()
-  const msgArray = msg.split('to')
+  const msgArray = msg.split(' ')
   if (msgArray.length === 1)
     return await ctx.reply(
-      "Please make sure that the 'to' keyword is lowercase not like this 'To' or 'tO'"
+      'Please make sure that you already put the needed time action'
     )
   const link = msgArray[0].trim()
   const time = msgArray[1].trim()
@@ -35,7 +35,7 @@ bot.command('retiming', async (ctx) => {
     !time.toLowerCase().includes('h')
   )
     return await ctx.reply(
-      "Dude! the time you want must contains (m s h), consider the following:\n• hour = h \n• minute = m \n• second = s\n if you want the link directive you to the first minute at the 14th second just put like that '1m14s'"
+      "Dude! the time you want must contains (m, s, h), consider the following:\n• hour = h \n• minute = m \n• second = s\n if you want the link directive you to the first minute at the 14th second just put like that '1m14s'"
     )
 
   return await ctx.reply(`${link}?t=${time}`)
